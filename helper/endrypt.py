c42921554841c0a12d23c3d59b26b084e34bf356
Saurav Gupta <sauravgupta108@gmail.com>
2019-06-20 21:46:46 +0530

    Update endrypt.py

--- helper/endrypt.py
+++ helper/endrypt.py
@@ -4,21 +4,37 @@ import json
 
 class HideAndSeek:
 	'''
-	This class encrypts or decrypts a string
+	This class encrypts or decrypts signals
 	'''
 
-	def __init__(self, string):
-		assert isinstance(string, str)
-		self.message = string
+	def __init__(self):
+		pass
 
-	def encrypt(self):
-		return Fernet(self.get_key()).encypt(self.message.encode())
+	def encrypt(self, raw_signal):
+		assert isinstance(raw_signal, str)
+		return Fernet(self.get_key()).encypt(raw_signal.encode())
 
-	def decrypt(self):
-		return Fernet(self.get_key()).decrypt(self.message).decode()
+	def decrypt(self, signal):
+		assert isinstance(signal, str)
+		return Fernet(self.get_key()).decrypt(signal).decode()
 
 	def get_key(self):
 		'''
-		Returns encoded secret key from configuration file
+		Returns secret key from configuration file
 		'''
-		pass
+		secert_key = None
+		try:
+			scrt = open('/opt/app/arduino_app/CutiePi/secret/security.json', 'r')
+			secert_key = json.loads(scrt.read())["secret_key"]			
+		
+		except FileNotFoundError: 
+			# TODO: log error
+			secert_key = None
+			
+		except Exception as e:
+			# TODO: log error
+			secert_key = None
+					
+		finally:
+			scrt.close()
+			return secret_key
