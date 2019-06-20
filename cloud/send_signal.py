40261dfda08e1013f5d3c22153d850bf4a08fe79
Saurav Gupta <sauravgupta108@gmail.com>
2019-06-20 21:45:48 +0530

    Update send_signal.py

--- cloud/send_signal.py
+++ cloud/send_signal.py
@@ -1,6 +1,20 @@
+from threading import Thread
+
+from .cloud_engine import CloudClient
+
+
 class SignalTransmission:
 	"""
 	This class sends signals to cloud via MQTT protocol.
+	get message >> encrypt >> send
 	"""
 	def __init__(self):
-		pass
+		client = CloudClient("send_msg", "SEND")
+
+	def send_signal(self, raw_signal):
+		'''encrypt'''
+		# log Preparing to send signal
+		from ..helper import endrypt
+		encrypted_msg = endrypt.HideAndSeek().encrypt(raw_signal)
+		# log message encrypted
+		client.transmit(encrypted_msg)
